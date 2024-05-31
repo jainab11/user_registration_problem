1b715ac0944faf6156b481abc71dcc26a9b52a9c
jainab11 <shaikhjainab11@gmail.com>
2024-05-31 16:16:42 +0530

    [Jainab] Add: validation for first name

---
+++ user_registration.py
@@ -0,0 +1,29 @@
+import re
+import logging
+class UserRegistration:
+    
+    def __init__(self):
+        self.first_name = None
+        self.logger = logging.getLogger(__name__) #loger instance
+        
+    def first_name_input(self):
+        while True:
+            first_name = input("Enter your first name : ")
+            if re.match("^[A-Z][a-z].{2,}$",first_name):
+                self.first_name = first_name
+                self.logger.info({first_name})
+                self.logger.info("Your first name is : %s",first_name)
+                break
+                
+            else:
+                self.logger.error("First letter needs to be capital and name should be more that three characters %s",first_name)
+            
+def main():
+    # logger
+    logging.basicConfig( filename= 'user.log',level=logging.INFO)
+    user_registration = UserRegistration()
+    user_registration.first_name_input()
+    
+    
+if __name__ == "__main__":
+    main()
